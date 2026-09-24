55d9e7b0d774a69b68280b9cf8d4fda8fd212678
TALHA <mdhezbullah330@gmail.com>
2026-09-24 10:15:31 +0600

    Delete app directory

--- app/api/logout/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { clearSession } from '../../../lib/session';
-
-export async function GET(request) {
-  await clearSession();
-  return NextResponse.redirect(new URL('/', request.url));
-}
